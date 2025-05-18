a6ea61beffe1d377d7561e7ae435631b7f2b1d04
Mykola Srybnik <mykola.srybnik@gmail.com>
2025-05-18 12:13:14 +0300

    Add task 3

--- Program.cs
+++ Program.cs
@@ -4,53 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Task 1
-        int[] nums = new int[50];
-
-        for (int i = 0; i < nums.Length; i++)
-        {
-            nums[i] = i * 2;
-        }
-
-        foreach (int numbers in nums)
-        {
-            Console.WriteLine($"Element: {numbers}");
-        }
-        // End of Task 1
-        
-        
-        // Task 2
-        int[] number = new int[33];
-        int evenNumbersCounter = 0;
-        List<int> evenNumbers = new List<int>();
-        
-        Random random_number = new Random();
-
-        for (int i = 0; i < number.Length; i++)
-        {
-            number[i] = random_number.Next(0, 100);
-        }
-
-        for(int i = 0; i < number.Length; i++)
-        {
-            if (number[i] % 2 == 0)
-            {
-                evenNumbersCounter++;
-                evenNumbers.Add(number[i]);
-            }
-        }
-        
-        Console.WriteLine($"Array includes {evenNumbersCounter} even numbers.");
-        Console.Write("It's: ");
-        
-        for(int i = 0; i < evenNumbers.Count; i++)
-        {
-            Console.Write(evenNumbers[i]);
-            if(i != evenNumbers.Count - 1)
-                Console.Write(", ");
-        }
-        // End of Task 2
-        
         //Task 3
         string userInput;
         string answer;
